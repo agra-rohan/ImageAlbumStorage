b5642974ea9124e59de5cf3277560f5780eb94ee
agra-rohan <ragrawal787@gmail.com>
2020-07-19 15:56:21 +0530

    ImageStorageAlbum

---
+++ go.mod
@@ -0,0 +1,8 @@
+module Projects/go-assignment
+
+go 1.14
+
+require (
+	github.com/gorilla/mux v1.7.4
+	go.mongodb.org/mongo-driver v1.4.0-rc0
+)
---
+++ main.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+type Album struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name      string             `json:"name" bson:"name"`
+	ImagePath string             `json:"ipath" bson:"ipath"`
+}
+
+func CreateAlbum(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var album Album
+	if err := json.NewDecoder(r.Body).Decode(&album); err != nil {
+		fmt.Println("error while decoding")
+		log.Fatal(err)
+	}
+	fmt.Println(album.Name)
+
+	collections, _ := client.Database("ImageAlbumStorage").ListCollectionNames(context.TODO(), bson.D{})
+
+	fmt.Println(collections)
+	for _, name := range collections {
+		if album.Name == name {
+			fmt.Println("Album already exists")
+			return
+		}
+	}
+	client.Database("ImageAlbumStorage").CreateCollection(context.TODO(), album.Name)
+	fmt.Println("Created Album")
+}
+
+func DeleteAlbum(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var album Album
+	if err := json.NewDecoder(r.Body).Decode(&album); err != nil {
+		fmt.Println("error while decoding")
+		log.Fatal(err)
+	}
+	fmt.Println(album.Name)
+
+	collections, _ := client.Database("ImageAlbumStorage").ListCollectionNames(context.TODO(), bson.D{})
+	fmt.Println(collections)
+
+	for _, name := range collections {
+		if album.Name == name {
+			if err := client.Database("ImageAlbumStorage").Collection(name).Drop(context.TODO()); err != nil {
+				log.Fatal("Error while deleting Album")
+			}
+			fmt.Printf("Deleted the album : ", name)
+			return
+		}
+	}
+	fmt.Println("Album does not exists")
+}
+
+func CreateImageInAlbum(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var album Album
+	if err := json.NewDecoder(r.Body).Decode(&album); err != nil {
+		fmt.Println("error while decoding")
+		log.Fatal(err)
+	}
+	fmt.Println(album.Name)
+	// Open file on disk.
+	f, _ := os.Open(album.ImagePath)
+
+	// Read entire JPG into byte slice.
+	reader := bufio.NewReader(f)
+	content, _ := ioutil.ReadAll(reader)
+
+	// Encode as base64.
+	encodedImage := base64.StdEncoding.EncodeToString(content)
+
+	fmt.Println("ENCODED: " + encodedImage)
+	client.Database("ImageAlbumStorage").Collection(album.Name).InsertOne(context.TODO(), album)
+
+}
+
+func DeleteImageInAlbum(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	fmt.Println(id)
+	var album Album
+	if err := json.NewDecoder(r.Body).Decode(&album); err != nil {
+		fmt.Println("error while decoding")
+		log.Fatal(err)
+	}
+
+	res, err := client.Database("ImageAlbumStorage").Collection(album.Name).DeleteOne(context.TODO(), bson.D{{"_id", id}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res.DeletedCount)
+
+}
+
+func ListImagesInAlbum(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var album Album
+	if err := json.NewDecoder(r.Body).Decode(&album); err != nil {
+		fmt.Println("error while decoding")
+		log.Fatal(err)
+	}
+
+	cursor, err := client.Database("ImageAlbumStorage").Collection(album.Name).Find(context.TODO(), bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var results []bson.M
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		log.Fatal(err)
+	}
+	for _, result := range results {
+		fmt.Println(result)
+	}
+}
+
+func GetImageInAlbum(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var album Album
+	if err := json.NewDecoder(r.Body).Decode(&album); err != nil {
+		fmt.Println("error while decoding")
+		log.Fatal(err)
+	}
+	params := mux.Vars(r)
+	id, err := primitive.ObjectIDFromHex(params["id"])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var result bson.M
+
+	client.Database("ImageAlbumStorage").Collection(album.Name).FindOne(context.TODO(), bson.D{{"_id", id}}).Decode(&result)
+	fmt.Println(result)
+}
+
+func main() {
+	fmt.Println("Starting Application")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, _ = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatal("Not able to Connect to mongo db")
+	}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/album", CreateAlbum).Methods("POST")
+	router.HandleFunc("/album", DeleteAlbum).Methods("DELETE")
+	router.HandleFunc("/album/image", CreateImageInAlbum).Methods("POST")
+	router.HandleFunc("/album/image/{id}", DeleteImageInAlbum).Methods("DELETE")
+	router.HandleFunc("/album/image", ListImagesInAlbum).Methods("GET")
+	router.HandleFunc("/album/image/{id}", GetImageInAlbum).Methods("GET")
+
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatal("Fail to Listenandserver")
+	}
+}
